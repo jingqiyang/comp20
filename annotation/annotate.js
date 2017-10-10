bea42ba6c6395a9692d7f96ca8727240f509dcd1
jingqi <jingqi.yang@tufts.edu>
2017-10-09 22:06:05 -0400

    added timestamp for each line of lyric in comments

--- annotation/annotate.js
+++ annotation/annotate.js
@@ -3,47 +3,46 @@ caps = document.getElementById("lyrics");
 
 video.addEventListener("timeupdate", function()
 {
-//	   Children have You ever met the Bogeyman before
-//	   No, of course You haven't for You're much too good, I'm sure;
-//	   Don't You be afraid of him if he should visit You,
-//	   He's a great big coward, so I'll tell You what to do
+//	   10-14 Children have You ever met the Bogeyman before
+//	   14-17.5 No, of course You haven't for You're much too good, I'm sure;
+//	   17.5-21.5 Don't You be afraid of him if he should visit You,
+//	   21.5-26 He's a great big coward, so I'll tell You what to do
 //
-//	   Hush, hush, hush, here comes the Bogeyman,
-//	   Don't let him come too close to You, he'll catch you if he can.
-//	   Just pretend that you're a crocodile
-//	   And you will find that Bogeyman will run away a mile.
+//	   26-29 Hush, hush, hush, here comes the Bogeyman,
+//	   29-33.5 Don't let him come too close to You, he'll catch you if he can.
+//	   33.5-37.5 Just pretend that you're a crocodile
+//	   37.5-41.5 And you will find that Bogeyman will run away a mile.
 //
-//	   Say Shoo shoo and stick him with a pin
-//	   Bogeyman will very nearly jump out of his skin
-//	   Say buzz buzz just like the wasps that sting
-//	   Bogeyman will think you are an elephant with wings
+//	   41.5-45.5 Say Shoo shoo and stick him with a pin
+//	   45.5-49.5 Bogeyman will very nearly jump out of his skin
+//	   49.5-53 Say buzz buzz just like the wasps that sting
+//	   53-57.5 Bogeyman will think you are an elephant with wings
 //
-//	   Hush, hush, hush, here comes the Bogeyman
-//	   Tell him you've got soldiers in your bed
-//	   For he will never guess that they are only made of lead
+//	   57.5-61 Hush, hush, hush, here comes the Bogeyman
+//	   65-68.5 Tell him you've got soldiers in your bed
+//	   68.5-72 For he will never guess that they are only made of lead
 //
-//	   Say Hush hush, he'll think that you're asleep
-//	   If you make a lovely snore away he'll softly creep
-//	   Sing this tune you children one and all
-//	   Bogeyman will run away, he'll think it's Henry Hall!
+//	   80.5-84.5 Say Hush hush, he'll think that you're asleep
+//	   84.5-88 If you make a lovely snore away he'll softly creep
+//	   88-92 Sing this tune you children one and all
+//	   92-96 Bogeyman will run away, he'll think it's Henry Hall!
 //
-//	   When the shadows of the evening creep across the sky
-//	   And your Mummy comes upstairs to sing a lullaby
-//	   Tell her that the bogeyman no longer frightens you
-//	   Uncle Henry's very kindly told you what to do
+//	   96-100 When the shadows of the evening creep across the sky
+//	   100-104 And your Mummy comes upstairs to sing a lullaby
+//	   104-108 Tell her that the bogeyman no longer frightens you
+//	   108-111.5 Uncle Henry's very kindly told you what to do
 //
-//	   Hush, hush, hush, here comes the Bogeyman
-//	   Don't let him come too close to you, He'll catch you if he can.
-//	   Just pretend your teddy bear's a dog
-//	   Then shout out, 'Fetch him, Teddy!' and he'll hop off like a frog.
+//	   111.5-115.5 Hush, hush, hush, here comes the Bogeyman
+//	   115.5-119.5 Don't let him come too close to you, He'll catch you if he can.
+//	   119.5-123 Just pretend your teddy bear's a dog
+//	   123-126.5 Then shout out, 'Fetch him, Teddy!' and he'll hop off like a frog.
 //
-//	   Say Meoow, pretend that you're a cat
-//	   He'll think you may scratch him that make him fall down flat
-//	   Just pretend he isn't really there
-//	   You will find that Bogey man will vanish in thin air
+//	   135-139 Say Meoow, pretend that you're a cat
+//	   139-143 He'll think you may scratch him that make him fall down flat
+//	   143-147 Just pretend he isn't really there
+//	   147-150.5 You will find that Bogey man will vanish in thin air
 //
-//	   Here's one way to catch him without fail
-//	   Just keep a little salt with you
-//	   and put it on his tail
+//	   159-162.5 Here's one way to catch him without fail
+//	   162.5-166 Just keep a little salt with you and put it on his tail
 
 }, false);
