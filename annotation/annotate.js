8f35b02d9a638efdff3f6fb6ad24ec6390e0c855
jingqi <jingqi.yang@tufts.edu>
2017-10-09 21:42:35 -0400

    added commented out lyrics for reference

--- annotation/annotate.js
+++ annotation/annotate.js
@@ -3,5 +3,47 @@ caps = document.getElementById("lyrics");
 
 video.addEventListener("timeupdate", function()
 {
+//	   Children have You ever met the Bogeyman before
+//	   No, of course You haven't for You're much too good, I'm sure;
+//	   Don't You be afraid of him if he should visit You,
+//	   He's a great big coward, so I'll tell You what to do
+//
+//	   Hush, hush, hush, here comes the Bogeyman,
+//	   Don't let him come too close to You, he'll catch you if he can.
+//	   Just pretend that you're a crocodile
+//	   And you will find that Bogeyman will run away a mile.
+//
+//	   Say Shoo shoo and stick him with a pin
+//	   Bogeyman will very nearly jump out of his skin
+//	   Say buzz buzz just like the wasps that sting
+//	   Bogeyman will think you are an elephant with wings
+//
+//	   Hush, hush, hush, here comes the Bogeyman
+//	   Tell him you've got soldiers in your bed
+//	   For he will never guess that they are only made of lead
+//
+//	   Say Hush hush, he'll think that you're asleep
+//	   If you make a lovely snore away he'll softly creep
+//	   Sing this tune you children one and all
+//	   Bogeyman will run away, he'll think it's Henry Hall!
+//
+//	   When the shadows of the evening creep across the sky
+//	   And your Mummy comes upstairs to sing a lullaby
+//	   Tell her that the bogeyman no longer frightens you
+//	   Uncle Henry's very kindly told you what to do
+//
+//	   Hush, hush, hush, here comes the Bogeyman
+//	   Don't let him come too close to you, He'll catch you if he can.
+//	   Just pretend your teddy bear's a dog
+//	   Then shout out, 'Fetch him, Teddy!' and he'll hop off like a frog.
+//
+//	   Say Meoow, pretend that you're a cat
+//	   He'll think you may scratch him that make him fall down flat
+//	   Just pretend he isn't really there
+//	   You will find that Bogey man will vanish in thin air
+//
+//	   Here's one way to catch him without fail
+//	   Just keep a little salt with you
+//	   and put it on his tail
 
-    }, false);
+}, false);
