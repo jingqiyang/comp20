74aab65bd9a1d0f6efe77d21bccddd42605e3ca1
jingqi <jingqi.yang@tufts.edu>
2017-10-20 10:44:50 -0400

    added friend info windows

--- landmarks/geo.js
+++ landmarks/geo.js
@@ -71,52 +71,98 @@ function renderMap()
     var meIcon = "me-emoji.png", friendIcon = "friend-emoji.png", landmarkIcon = "landmark-emoji.png";
     
     //console.log(locations);
-
-    //create friend markers
-    for (i = 0; i < locations["people"].length; i++)
-    {
-        //if person is not user
-        if (locations["people"][i]["login"] != "7QNJ31fE")
-        {
-            friend = {lat: (locations["people"])[i]["lat"], lng: (locations["people"])[i]["lng"]};
-         
-            friendMarker = new google.maps.Marker({
-                position: friend,
-                map: map,
-                title: "friend!",
-                icon: friendIcon
-            });
-        }
-    }
-
-    //create landmark markers
-    for (i = 0; i < locations["landmarks"].length; i++)
-    {
-        landmark =
-        {
-            lat: (locations["landmarks"])[i]["geometry"]["coordinates"][1],
-            lng: (locations["landmarks"])[i]["geometry"]["coordinates"][0]
-        };
-
-        landMarker = new google.maps.Marker({
-            position: landmark,
-            map: map,
-            title: "landmark!",
-            icon: landmarkIcon
-        });
-    }
     
-    //create user marker after landmark & friend markers are set
+    //create user marker
     myMarker = new google.maps.Marker({
         position: me,
         map: map,
         title: "Me!",
         icon: meIcon
     });
-    
+
     //user marker info window
-    google.maps.event.addListener(myMarker, 'click', function() {
+    google.maps.event.addListener(myMarker, 'click', function()
+    {
         infoWindow.setContent(myMarker.title);
         infoWindow.open(map, myMarker);
     });
+    
+    //create friend markers
+    for (i = 0; i < locations["people"].length; i++)
+        addFriendMarker(i, friendIcon);
+    
+    //create landmark markers
+    for (i = 0; i < locations["landmarks"].length; i++)
+        addLandMarker(i, landmarkIcon);
+}
+
+//create friend marker
+function addFriendMarker(i, friendIcon)
+{
+    //if person is not user
+    if (locations["people"][i]["login"] != "7QNJ31fE")
+    {
+        var friendLat = locations["people"][i]["lat"];
+        var friendLng = locations["people"][i]["lng"];
+        friend = {lat: friendLat, lng: friendLng};
+        
+        //distance = google.maps.geometry.spherical.computeDistanceBetween(me, friend) / 1609.34;
+        
+        distance = calcDistance(myLat, myLng, friendLat, friendLng);
+        
+        friendMarker = new google.maps.Marker({
+            position: friend,
+            map: map,
+            icon: friendIcon
+        });
+        
+        friendMarker.content = "login: " + locations["people"][i]["login"] + "<br />" + "distance: " + distance + " miles";
+        
+        //friendMarker.infoWindow = new google.maps.InfoWindow();
+        
+        //friend marker info window
+        google.maps.event.addListener(friendMarker, 'click', function()
+        {
+            infoWindow.setContent(this.content);
+            infoWindow.open(map, this);
+        });
+    }
+}
+
+//create landmark marker
+function addLandMarker(i, landmarkIcon)
+{
+    var landmarkLat = locations["landmarks"][i]["geometry"]["coordinates"][1];
+    var landmarkLng = locations["landmarks"][i]["geometry"]["coordinates"][0];
+    
+    landmark = {lat: landmarkLat, lng: landmarkLng};
+
+    landMarker = new google.maps.Marker({
+        position: landmark,
+        map: map,
+        icon: landmarkIcon
+    });
+}
+
+//convert to radians
+Number.prototype.toRad = function()
+{
+    return this * Math.PI / 180;
+}
+
+//calculate distance in miles between 2 geolocations
+function calcDistance(lat1, lng1, lat2, lng2)
+{
+    var x1 = lat2 - lat1;
+    var dLat = x1.toRad();
+    var x2 = lng2 - lng1;
+    var dLng = x2.toRad();
+    
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+    Math.sin(dLng/2) * Math.sin(dLng/2);
+    
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    return c * 3.9587657052;    //miles
 }
