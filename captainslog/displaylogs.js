09c5b2e0412af0fbba5ab75ccb626814db119956
jingqi <jingqi.yang@tufts.edu>
2017-11-01 12:59:37 -0400

    cleaned up functions & removed console.log

--- captainslog/displaylogs.js
+++ captainslog/displaylogs.js
@@ -2,20 +2,30 @@ $(document).ready(function()
 {
     $("#msg").change(function()
     {
-        //store new entry in local storage
-        var date = new Date();
-        var key = date.getTime();
-        var month = date.getMonth() + 1;
-        var value = "<p>" + date.getHours() + ":" +
-                    date.getMinutes() + " " +
-                    month + "/" +
-                    date.getDate() + "/" +
-                    date.getFullYear() + " - " +
-                    $("#msg").val() + "</p>";
-        console.log(value);
-        localStorage.setItem(key, value);
-                    
-        //load all logs
-        
+        store();    //store in local storage
+        load();      //load all logs
     });
 });
+
+//store new entry in local storage
+function store()
+{
+    var date = new Date();
+    var key = date.getTime();
+    
+    var month = date.getMonth() + 1;
+    var value = "<p>" + date.getHours() + ":" +
+    date.getMinutes() + " " +
+    month + "/" +
+    date.getDate() + "/" +
+    date.getFullYear() + " - " +
+    $("#msg").val() + "</p>";
+    
+    localStorage.setItem(key, value);
+}
+
+//load logs in reverse chronological order
+function load()
+{
+    
+}
