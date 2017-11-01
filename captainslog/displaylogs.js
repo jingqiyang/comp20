c227e073a5534e8768ad9c2480f69091d37b0bd9
jingqi <jingqi.yang@tufts.edu>
2017-11-01 12:56:46 -0400

    changed stored value to date + msg string

--- captainslog/displaylogs.js
+++ captainslog/displaylogs.js
@@ -1,7 +1,21 @@
 $(document).ready(function()
 {
-    $("#msg").change(function() {
-        key = new Date().getTime();
-        localStorage.setItem(key, $("#msg").val());
+    $("#msg").change(function()
+    {
+        //store new entry in local storage
+        var date = new Date();
+        var key = date.getTime();
+        var month = date.getMonth() + 1;
+        var value = "<p>" + date.getHours() + ":" +
+                    date.getMinutes() + " " +
+                    month + "/" +
+                    date.getDate() + "/" +
+                    date.getFullYear() + " - " +
+                    $("#msg").val() + "</p>";
+        console.log(value);
+        localStorage.setItem(key, value);
+                    
+        //load all logs
+        
     });
 });
